6f3fce5f003f4e57acb31c7e4b4f5606dd4c2eec
Rodrigo Cardoso <rodrigo.cardoso1007@gmail.com>
2025-03-10 12:25:05 -0300

    Refactor: Refactoring CardsService

---
+++ src/services/Cards/Cards.ts
@@ -0,0 +1,38 @@
+import { ICardTyped, IFormCardData } from "../../@types";
+import { ICard } from "../../model";
+import { RestApi, Uuid } from "../../modules";
+
+const CARD_SERVICE = '/cards';
+
+export async function getCards(): Promise<ICardTyped[]>{
+  const res = await RestApi.httpGet<ICard[]>(CARD_SERVICE);
+
+  return res.map((card, idx) => ({
+    id: card.id,
+    ownerName: card.name,
+    cardNumber: card.number,
+    securityCode: card.cvv,
+    expirationDate: card.expirationDate,
+    type: (idx % 2) ? 'black' : 'green'
+  }))
+}
+
+export async function createCards(card: IFormCardData): Promise<ICardTyped> {
+  const cardDto: ICard = {
+    id: Uuid.generateUUid(),
+    number: card.cardNumber,
+    name: card.ownerName,
+    expirationDate: card.expirationDate,
+    cvv: card.securityCode,
+  };
+
+  const res = await RestApi.httpPost<ICard>(CARD_SERVICE, cardDto);
+
+  return {
+    ...res,
+    type: 'black',
+    cardNumber: res.number,
+    securityCode: res.cvv,
+    ownerName: res.name
+  }
+}
